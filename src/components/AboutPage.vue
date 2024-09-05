726f5d7868e7d1d32a358dc74629e0203b091d90
jojobot <adenijimujeeb8@gmail.com>
2024-09-05 10:22:53 +0100

    setting

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is the about page of your Vue app.</p>
+    <router-link to="/">Go back to Home Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #ff9800;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Welcome to the Home Page</h1>
+    <p>This is your first Vue.js page.</p>
+    <router-link to="/about">Go to About Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
